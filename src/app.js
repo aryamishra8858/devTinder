2a3fc720f38a4be9e04e15c934161924fd7fb617
aryamishra8858 <aryamishra8858@gmail.com>
2025-07-23 01:22:57 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,36 +1,11 @@
 const express = require("express");
 const app = express();
 
-// app.use ("/hello",(req,res)=>{
-//     res.send("Hello World");
-// });
-
-app.use("/user", (req,res)=>{
-    res.send("Hello from the user route");
-});
-
 //This will only handle GET call to /user
-app.get("/user", (req,res)=>{
+app.get("/user/:userId/:name/:password", (req,res)=>{
+    console.log(req.params);
     res.send ({firstName: "Arya", lastName: "Mishra", age: 20});
 });
-
-app.post("/user", (req,res)=>{
-//saving to data to database
-res.send ("Data successfully saved to the database");
-});
-
-app.delete("/user", (req,res)=>{
-    res.send ("Deleted successfully");
-})
-
-//thia will match all the http method API calls to /test
-app.use("/test", (req, res)=>{
-    res.send("Hello from the server");
-});
-
-// app.use("/", (req,res)=>{
-//     res.send("Welcome to the Dev Tinder");
-// })
 
 app.listen(7000, () =>{
     console.log("Server is running on port 7000");
