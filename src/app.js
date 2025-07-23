e753cdf4cc5615c98e8d6e013a87782d25d567db
aryamishra8858 <aryamishra8858@gmail.com>
2025-07-24 01:57:00 +0530

    middleware an d request handlers

--- src/app.js
+++ src/app.js
@@ -1,31 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", 
-    (req,res,next)=>{
-    console.log("Handling the route user");
-    // res.send("Route Handler 1");
-    next(); // Call the next middleware or route handler
-    },
-    (req, res,next) => {
-        // res.send("Route Handler 2");
-        console.log("Handling the route user 2");
-     // Call the next middleware or route handler
-     next();
-    },
-        (req, res,next) => {
-        // res.send("Route Handler 3");
-        console.log("Handling the route user 3");
-     // Call the next middleware or route handler
-     next();
-    },
-        (req, res,next) => {
-        res.send("Route Handler 4");
-        console.log("Handling the route user 4");
-     // Call the next middleware or route handler
-    }
-); 
+//Get /users => Middleware chain => request handler
 
-app.listen(7000, () =>{
-    console.log("Server is running on port 7000");
+app.use("/", (req, res, next) => {
+//   res.send("Route Handler 1");
+  next(); // Call the next middleware or route handler
+});
+
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route user1");
+    // res.send("Route Handler 2");
+    next(); // Call the next middleware or route handler
+  },
+  (req, res, next) => {
+    res.send("Route Handler 3");
+  },
+  (req, res, next) => {
+    res.send("Route Handler 4");
+  }
+);
+
+app.listen(7000, () => {
+  console.log("Server is running on port 7000");
 });
