f803f840c733d9303fb92616ec39d95a6972c83c
aryamishra8858 <aryamishra8858@gmail.com>
2025-07-23 19:18:15 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const app = express();
 
-//This will only handle GET call to /user
-app.get("/user/:userId/:name/:password", (req,res)=>{
-    console.log(req.params);
-    res.send ({firstName: "Arya", lastName: "Mishra", age: 20});
-});
+app.use("/user", 
+    (req,res,next)=>{
+    console.log("Handling the route user");
+    // res.send("Route Handler 1");
+    next(); // Call the next middleware or route handler
+    },
+    (req, res,next) => {
+        // res.send("Route Handler 2");
+        console.log("Handling the route user 2");
+     // Call the next middleware or route handler
+     next();
+    },
+        (req, res,next) => {
+        // res.send("Route Handler 3");
+        console.log("Handling the route user 3");
+     // Call the next middleware or route handler
+     next();
+    },
+        (req, res,next) => {
+        res.send("Route Handler 4");
+        console.log("Handling the route user 4");
+     // Call the next middleware or route handler
+    }
+); 
 
 app.listen(7000, () =>{
     console.log("Server is running on port 7000");
