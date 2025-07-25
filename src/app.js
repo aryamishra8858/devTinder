b974b7ab3c3c144982341f123092a045e08ef32d
aryamishra8858 <aryamishra8858@gmail.com>
2025-07-26 01:16:40 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -6,19 +6,39 @@ const User = require("./models/user");
 app.use(express.json()); // Middleware to parse JSON bodies
 
 app.post("/signup", async (req,res)=>{
-  //Creating a new instance of the user model
-  const user = new User (req.body);
- 
-try{
-  await user.save();
-res.send("User created successfully");
-} catch(error) {
-  res.status(500).send("Internal Server Error" + error.message);
+});
+
+// Get user by emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try{
+    const users = await User.findOne();
+    if (users.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(users);
+    } 
+  } catch (error) {
+    res.status(404).send("Something went wrong: " + error.message);
+  }
 }
+);
+
+
+// Feed API - GET/feed - get all the users from database
+app.get("/feed",async (req,res) => {
+
+  try{
+ const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(404).send("Something went wrong: " + error.message);
+  }
 });
 
 
-connectDB()
+connectDB() 
   .then(() => {
     console.log("MongoDB connected successfully");
     app.listen(7000, () => {
