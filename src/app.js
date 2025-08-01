eea32b6d885caf17a0fc3ec28b808f6196c7f898
aryamishra8858 <aryamishra8858@gmail.com>
2025-08-01 16:21:37 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -5,47 +5,69 @@ const User = require("./models/user");
 
 app.use(express.json()); // Middleware to parse JSON bodies
 
-app.post("/signup", async (req,res)=>{
-});
+app.post("/signup", async (req, res) => {});
 
 // Get user by emailId
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
 
-  try{
+  try {
     const users = await User.findOne();
     if (users.length === 0) {
       res.status(404).send("User not found");
     } else {
       res.send(users);
-    } 
+    }
   } catch (error) {
     res.status(404).send("Something went wrong: " + error.message);
   }
-}
-);
-
+});
 
 // Feed API - GET/feed - get all the users from database
-app.get("/feed",async (req,res) => {
-
-  try{
- const users = await User.find({});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
     res.send(users);
   } catch (error) {
     res.status(404).send("Something went wrong: " + error.message);
   }
 });
 
+// Delete user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (error) {
+    res.status(404).send("Something went wrong: " + error.message);
+  }
+});
 
-connectDB() 
+//Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  // console.log(data);
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runvalidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(404).send("Something went wrong: " + error.message);
+  }
+});
+
+connectDB()
   .then(() => {
     console.log("MongoDB connected successfully");
     app.listen(7000, () => {
       console.log("Server is running on port 7000");
     });
   })
-  .catch(error => {
+  .catch((error) => {
     console.error("MongoDB connection error:", error);
     // Exit the process with failure
   });
--- src/models/user.js
+++ src/models/user.js
@@ -2,21 +2,52 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     firstName: {
-        type: String},
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 50,
+    },
     lastName: {
-        type: String},
-        emailId: {
-            type:String
-        },
-        password: {
-            type: String
-        },
-        age: {
-            type: Number
-        },
-        gender: {
-            type: String
-        },
-    });
+        type: String
+    },
+    emailId: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String
+    },
+    age: {
+        type: Number,
+        min: 18,
+        max: 50,
+    },
+    gender: {
+        type: String,
+        validate(value) {
+            if (!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender data is not valid");
+            }
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: "https://example.com/default-profile.png",
+    },
+    about: {
+        type: String,
+        default: "No information provided",
+    },
+    skills: {
+        type: [String]
+    }
+},
+{
+    timestamps: true // Automatically add createdAt and updatedAt fields
+}
+);
 
-    module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", userSchema);
